a2ea74e9d7475655ac8a8bd49f07422a70267303
myrna12345 <myrnathiara23@mhs.politala.ac.id>
2024-11-26 19:42:21 +0800

    upload

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,3 @@ Route::resource('/rekap_masuks', \App\Http\Controllers\Rekap_MasukController::cl
 //route resource for products
 Route::resource('/rekap_keluars', \App\Http\Controllers\Rekap_KeluarController::class);
 
-// Rute untuk halaman login
-Route::get('/login', function () {
-    return view('login'); // Halaman login
-})->name('login');
-
-// Rute untuk proses login
-Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
-
-// Rute untuk logout
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-// Rute untuk halaman register
-Route::get('/register', function () {
-    return view('register'); // Pastikan 'register' sesuai dengan nama view yang akan dibuat
-})->name('register');
-// Rute untuk proses registrasi
-Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
